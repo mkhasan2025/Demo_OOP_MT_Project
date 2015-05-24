03a6412588c8eda7031c1edcd9805128ccfa012f
mkhasan2025 <mkhasan2025@gmail.com>
2015-05-24 15:38:21 +0900

    20150524_1540_configparamClassDescription

--- Demo_OOP_MT_Project/SrcCode/ConfigParameter.h
+++ Demo_OOP_MT_Project/SrcCode/ConfigParameter.h
@@ -1,3 +1,11 @@
+/*
+ConfigParameter class contains all the basic configuration
+parameters of a the image/video frames. This is designed to
+be a singletone class so that only one instance of the class
+can be instantiated throughout the project and that single
+object will be used (modified, read) by all other classes.
+*/
+
 #ifndef __CONFIG_PARAM
 #define __CONFIG_PARAM
 
@@ -11,9 +19,16 @@ private:
 	int nOutImageWidth, nOutImageHeight;
 	char *pImageSrc, *pImageDst;
 
+	// As a singletone class, the constructor of this
+	// class has been declared as private to disable 
+	// the instantiation of this class via contructor.
 	ConfigParameter();
 
+	// cpInstanceFlag will be used to control the object
+	// instantiation of this singletone class.
 	static bool cpInstanceFlag;
+	// static member cpInstance denote the pointer to the
+	// only object that will be instantiated from this class 
 	static ConfigParameter *cpInstance;
 
 public:
@@ -34,6 +49,8 @@ public:
 	char* getImageSrc();
 	char* getImageDst();
 	
+	//Disable the default copy-constructor and copy-assignment-operator
+	//for this singletone class.
 	ConfigParameter(const ConfigParameter&) = delete;
 	ConfigParameter & operator=(const ConfigParameter&) = delete;
 };
